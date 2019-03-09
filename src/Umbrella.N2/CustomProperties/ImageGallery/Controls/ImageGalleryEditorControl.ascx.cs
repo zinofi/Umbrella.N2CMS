3018a0f748cd2263e079710fc1af115bdb084550
Richard Edwards <richard.edwards@zinofi.co.uk>
2019-03-09 19:40:27 +0000

    Initial commit of the code moved from the main Umbrella repo.

---
+++ src/Umbrella.N2/CustomProperties/ImageGallery/Controls/ImageGalleryEditorControl.ascx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using N2.Resources;
+using Umbrella.Legacy.WebUtilities.Controls;
+
+namespace Umbrella.N2.CustomProperties.ImageGallery.Controls
+{
+    [EmbeddedUserControlOptions(false, ClientIDMode.Static)]
+    public partial class ImageGalleryEditorControl : EmbeddedUserControl
+    {
+    }
+}
